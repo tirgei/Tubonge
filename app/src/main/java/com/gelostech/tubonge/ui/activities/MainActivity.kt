8c383c02a08e2201b080c149b63fde92e7131152
Vincent Tirgei <tirgeic@gmail.com>
2019-02-20 11:57:30 +0300

    Add main toolbar

--- app/src/main/java/com/gelostech/tubonge/ui/activities/MainActivity.kt
+++ app/src/main/java/com/gelostech/tubonge/ui/activities/MainActivity.kt
@@ -3,11 +3,20 @@ package com.gelostech.tubonge.ui.activities
 import android.os.Bundle
 import com.gelostech.tubonge.R
 import com.gelostech.tubonge.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = null
+
     }
 }
