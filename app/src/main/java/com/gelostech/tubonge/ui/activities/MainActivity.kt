d86763c4a746dc40511f2adb6fca0d16fb17b74c
Vincent Tirgei <tirgeic@gmail.com>
2019-02-20 13:01:11 +0300

    Initiate empty state

--- app/src/main/java/com/gelostech/tubonge/ui/activities/MainActivity.kt
+++ app/src/main/java/com/gelostech/tubonge/ui/activities/MainActivity.kt
@@ -3,7 +3,10 @@ package com.gelostech.tubonge.ui.activities
 import android.os.Bundle
 import com.gelostech.tubonge.R
 import com.gelostech.tubonge.ui.base.BaseActivity
+import com.gelostech.tubonge.utils.loadUrl
+import com.gelostech.tubonge.utils.show
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.empty_state.view.*
 
 class MainActivity : BaseActivity() {
 
@@ -12,11 +15,18 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
 
         initViews()
+        showEmptyState()
     }
 
     private fun initViews() {
         setSupportActionBar(toolbar)
         supportActionBar?.title = null
 
+        emptyState.image.loadUrl(R.drawable.empty_inbox)
+        emptyState.text.text = getString(R.string.empty_inbox)
+    }
+
+    private fun showEmptyState() {
+        emptyState.show()
     }
 }
