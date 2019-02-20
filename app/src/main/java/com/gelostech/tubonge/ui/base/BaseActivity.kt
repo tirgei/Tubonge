3cb7f9bf89b4663f39950cace164a333a321249d
Vincent Tirgei <tirgeic@gmail.com>
2019-02-20 11:38:25 +0300

    Create base activity

--- app/src/main/java/com/gelostech/tubonge/ui/base/BaseActivity.kt
+++ app/src/main/java/com/gelostech/tubonge/ui/base/BaseActivity.kt
@@ -1,12 +1,11 @@
-package com.gelostech.tubonge
+package com.gelostech.tubonge.ui.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
