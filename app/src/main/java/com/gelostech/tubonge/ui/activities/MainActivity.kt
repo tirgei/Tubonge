8bf2a82ded5e15d04ce3f3407ca993978efbf899
Vincent Tirgei <tirgeic@gmail.com>
2019-02-20 11:38:39 +0300

    Refactor package structure

---
+++ app/src/main/java/com/gelostech/tubonge/ui/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gelostech.tubonge.ui.activities
+
+import android.os.Bundle
+import com.gelostech.tubonge.R
+import com.gelostech.tubonge.ui.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
