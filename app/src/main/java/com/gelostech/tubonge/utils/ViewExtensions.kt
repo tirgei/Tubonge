4b3dfb8683c7054cc8f638fb9e7c4be4fdadef81
Vincent Tirgei <tirgeic@gmail.com>
2019-02-20 18:35:13 +0300

    Add extensions to add/replace fragments

--- app/src/main/java/com/gelostech/tubonge/utils/ViewExtensions.kt
+++ app/src/main/java/com/gelostech/tubonge/utils/ViewExtensions.kt
@@ -5,8 +5,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
@@ -49,4 +53,16 @@ fun CircleImageView.loadAvatar(url: Int) {
 // Extension to bind layout to recyclerview adapter
 inline fun <reified T : ViewDataBinding> ViewGroup.inflate(@LayoutRes layoutRes: Int): T {
     return DataBindingUtil.inflate(LayoutInflater.from(context), layoutRes, this, false)
+}
+
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+    beginTransaction().func().commit()
+}
+
+fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int){
+    supportFragmentManager.inTransaction { add(frameId, fragment) }
+}
+
+fun AppCompatActivity.replaceFragment(fragment: Fragment, frameId: Int) {
+    supportFragmentManager.inTransaction{ replace(frameId, fragment) }
 }
