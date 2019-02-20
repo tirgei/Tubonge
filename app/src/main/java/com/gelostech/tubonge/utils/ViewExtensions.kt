ddaa513c9a60b2b7b99226a306a1fdcfd6771f18
Vincent Tirgei <tirgeic@gmail.com>
2019-02-20 16:58:54 +0300

    Add extension for binding recyclerview item

--- app/src/main/java/com/gelostech/tubonge/utils/ViewExtensions.kt
+++ app/src/main/java/com/gelostech/tubonge/utils/ViewExtensions.kt
@@ -1,9 +1,17 @@
 package com.gelostech.tubonge.utils
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import com.gelostech.tubonge.R
+import de.hdodenhof.circleimageview.CircleImageView
 
 // Function to show hidden view
 fun View.show() {
@@ -24,4 +32,21 @@ fun ImageView.loadUrl(url: Int) {
         .transition(DrawableTransitionOptions.withCrossFade())
         .thumbnail(0.05f)
         .into(this)
+}
+
+// Function to load user avatar
+fun CircleImageView.loadAvatar(url: Int) {
+    val options = RequestOptions().placeholder(R.drawable.user_avatar).centerCrop()
+
+    Glide.with(this.context)
+        .load(url)
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .thumbnail(0.05f)
+        .apply(options)
+        .into(this)
+}
+
+// Extension to bind layout to recyclerview adapter
+inline fun <reified T : ViewDataBinding> ViewGroup.inflate(@LayoutRes layoutRes: Int): T {
+    return DataBindingUtil.inflate(LayoutInflater.from(context), layoutRes, this, false)
 }
