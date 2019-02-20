4e37c31b83f090fd662f8fe93b3181c9b2d099fb
Vincent Tirgei <tirgeic@gmail.com>
2019-02-20 11:56:04 +0300

    Set status bar color to white

--- app/src/main/java/com/gelostech/tubonge/ui/base/BaseActivity.kt
+++ app/src/main/java/com/gelostech/tubonge/ui/base/BaseActivity.kt
@@ -1,11 +1,19 @@
 package com.gelostech.tubonge.ui.base
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.gelostech.tubonge.utils.AppUtils
 
 open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (AppUtils.buildMarshMellow()) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            window.statusBarColor = Color.WHITE
+        }
     }
 }
