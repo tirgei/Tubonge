4de7cd1ec0d4a8eb2af79e9340ea8493222c7fec
Vincent Tirgei <tirgeic@gmail.com>
2019-02-20 18:34:59 +0300

    Add function to check back press

--- app/src/main/java/com/gelostech/tubonge/ui/base/BaseActivity.kt
+++ app/src/main/java/com/gelostech/tubonge/ui/base/BaseActivity.kt
@@ -3,10 +3,13 @@ package com.gelostech.tubonge.ui.base
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import com.gelostech.tubonge.utils.AppUtils
+import org.jetbrains.anko.toast
 
 open class BaseActivity : AppCompatActivity() {
+    private var doubleBackToExit = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +17,16 @@ open class BaseActivity : AppCompatActivity() {
         if (AppUtils.buildMarshMellow()) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             window.statusBarColor = Color.WHITE
+        }
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExit) {
+            super.onBackPressed()
+        } else {
+            toast("Tap back again to exit")
+            doubleBackToExit = true
+            Handler().postDelayed({ doubleBackToExit = false }, 1500)
         }
     }
 }
