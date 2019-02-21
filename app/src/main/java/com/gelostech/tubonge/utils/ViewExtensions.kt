224222beaf7c922caf8c8d4da77aa1eafa1c9ca4
Vincent Tirgei <tirgeic@gmail.com>
2019-02-21 13:18:40 +0300

    Add extension to set drawable to EditText

--- app/src/main/java/com/gelostech/tubonge/utils/ViewExtensions.kt
+++ app/src/main/java/com/gelostech/tubonge/utils/ViewExtensions.kt
@@ -1,8 +1,10 @@
 package com.gelostech.tubonge.utils
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
@@ -17,19 +19,19 @@ import com.bumptech.glide.request.RequestOptions
 import com.gelostech.tubonge.R
 import de.hdodenhof.circleimageview.CircleImageView
 
-// Function to show hidden view
+// Extension to show hidden view
 fun View.show() {
     if (!this.isShown)
         this.visibility = View.VISIBLE
 }
 
-// Function to hide shown view
+// Extension to hide shown view
 fun View.hide() {
     if (this.isShown)
         this.visibility = View.GONE
 }
 
-// Function to load image from Int resource
+// Extension to load image from Int resource
 fun ImageView.loadUrl(url: Int) {
     Glide.with(this.context)
         .load(url)
@@ -38,7 +40,7 @@ fun ImageView.loadUrl(url: Int) {
         .into(this)
 }
 
-// Function to load user avatar
+// Extension to load user avatar
 fun CircleImageView.loadAvatar(url: Int) {
     val options = RequestOptions().placeholder(R.drawable.user_avatar).centerCrop()
 
@@ -54,6 +56,12 @@ fun CircleImageView.loadAvatar(url: Int) {
 inline fun <reified T : ViewDataBinding> ViewGroup.inflate(@LayoutRes layoutRes: Int): T {
     return DataBindingUtil.inflate(LayoutInflater.from(context), layoutRes, this, false)
 }
+
+// Extension to set drawable to left of Edittext
+fun EditText.setDrawable(icon: Drawable) {
+    this.setCompoundDrawablesWithIntrinsicBounds(icon, null, null, null)
+}
+
 
 inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
     beginTransaction().func().commit()
