9f68267fe37edd702f9cb08502c45dd0c835918a
Vincent Tirgei <tirgeic@gmail.com>
2019-02-21 13:19:01 +0300

    Add function to generate iconics drawable

--- app/src/main/java/com/gelostech/tubonge/utils/AppUtils.kt
+++ app/src/main/java/com/gelostech/tubonge/utils/AppUtils.kt
@@ -1,11 +1,21 @@
 package com.gelostech.tubonge.utils
 
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.os.Build
+import androidx.core.content.ContextCompat
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.typeface.IIcon
 
 object AppUtils {
 
     // Check if Build version is >= Marshmallow
     fun buildMarshMellow(): Boolean {
         return Build.VERSION.SDK_INT > Build.VERSION_CODES.M
+    }
+
+    // Return iconics drawable
+    fun drawable(context: Context, icon: IIcon, color: Int, size: Int): Drawable {
+        return IconicsDrawable(context).icon(icon).color(ContextCompat.getColor(context, color)).sizeDp(size)
     }
 }
