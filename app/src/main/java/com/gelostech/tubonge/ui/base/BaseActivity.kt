96e60300bb8b8ae21f2731775233b167edb4da0a
Vincent Tirgei <tirgeic@gmail.com>
2019-02-21 11:02:28 +0300

    Remove onbackpress function

--- app/src/main/java/com/gelostech/tubonge/ui/base/BaseActivity.kt
+++ app/src/main/java/com/gelostech/tubonge/ui/base/BaseActivity.kt
@@ -1,15 +1,13 @@
 package com.gelostech.tubonge.ui.base
 
+import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.gelostech.tubonge.utils.AppUtils
-import org.jetbrains.anko.toast
 
 open class BaseActivity : AppCompatActivity() {
-    private var doubleBackToExit = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,13 +18,7 @@ open class BaseActivity : AppCompatActivity() {
         }
     }
 
-    override fun onBackPressed() {
-        if (doubleBackToExit) {
-            super.onBackPressed()
-        } else {
-            toast("Tap back again to exit")
-            doubleBackToExit = true
-            Handler().postDelayed({ doubleBackToExit = false }, 1500)
-        }
+    fun launchActivity(a: Class<*>) {
+        startActivity(Intent(this, a))
     }
 }
